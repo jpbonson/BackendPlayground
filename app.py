c67ef7d4b5e5ca5be5f67691faee578c458b9221
Jessica <jpbonson@gmail.com>
2016-08-20 02:08:00 -0300

    add simple db and a test for it

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from tinydb import TinyDB, Query
 
 app = Flask(__name__)
+
+db = TinyDB('db.json')
 
 @app.route("/")
 def hello():
--- tests/simple_tests.py
+++ tests/simple_tests.py
@@ -1,5 +1,6 @@
+import unittest
 from app import app
-import unittest 
+from tinydb import TinyDB, Query
 
 class SimpleTests(unittest.TestCase): 
 
@@ -13,18 +14,26 @@ class SimpleTests(unittest.TestCase):
 
     def setUp(self):
         self.app = app.test_client()
-        self.app.testing = True 
+        self.app.testing = True
 
     def tearDown(self):
         pass 
 
     def test_home_status_code(self):
         result = self.app.get('/') # sends HTTP GET request
-        self.assertEqual(result.status_code, 200) 
+        self.assertEqual(result.status_code, 200)
 
     def test_home_data(self):
         result = self.app.get('/') # sends HTTP GET request
-        self.assertEqual(result.data, "Hello World!") 
+        self.assertEqual(result.data, "Hello World!")
+
+    def test_working_db(self):
+        db_path = 'tests/db_test.json'
+        open(db_path, 'w').close()
+        db = TinyDB(db_path)
+        User = Query()
+        db.insert({'name': 'John', 'age': 22})
+        self.assertEqual(db.search(User.name == 'John'), [{u'age': 22, u'name': u'John'}])
 
 if __name__ == '__main__':
     unittest.main()
