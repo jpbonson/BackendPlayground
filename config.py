2d4103f5b721fa9dbc50453d090971cc9dc9cfb4
Jessica <jpbonson@gmail.com>
2016-08-22 22:32:28 -0300

    added a logger

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from flask.ext.cache import Cache 
 from tinydb import TinyDB
 
@@ -7,6 +8,8 @@ class BaseConfig(object):
     TESTING = False
     DATABASE = TinyDB('db.json')
     CACHE_TYPE = 'simple'
+    LOGGING_LOCATION = 'debug.log'
+    LOGGING_LEVEL = logging.DEBUG
 
 class ProductionConfig(BaseConfig):
     DATABASE = TinyDB('prod_db.json')
@@ -28,3 +31,4 @@ config = {
 def configure_app(app, config_name = os.getenv('FLASK_CONFIGURATION', 'default')):
     app.config.from_object(config[config_name])
     app.cache = Cache(app)
+    logging.basicConfig(filename=app.config['LOGGING_LOCATION'],level=app.config['LOGGING_LEVEL'])
