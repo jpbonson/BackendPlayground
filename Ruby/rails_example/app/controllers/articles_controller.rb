ebd5748cdf2edd295166f5bb2bb5c3b50781e873
jpbonson <jpbonson@gmail.com>
2017-07-19 13:40:46 -0300

    added basic auth

--- Ruby/rails_example/app/controllers/articles_controller.rb
+++ Ruby/rails_example/app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "user", password: "pass", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
