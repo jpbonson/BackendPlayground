54bf6b68c83a61f4db66bd3e3d5b94b7d6ef52a6
Jessica <jpbonson@gmail.com>
2018-09-14 19:39:12 -0300

    review go tutorial part 1

--- Golang/Tutorial/tutorial__part1__basic.go
+++ Golang/Tutorial/tutorial__part1__basic.go
@@ -17,6 +17,7 @@ var (
 )
 
 const Blah = 3.14
+const Bleh = "bleh"
 
 func add(x int, y int) int {
 	return x + y
@@ -62,7 +63,8 @@ func main() {
 	var i, j int = 1, 2
 	var q, w = true, "no!"
 	k := 3
-	fmt.Println("local vars ", i, j, q, w, k)
+	var blah string
+	fmt.Println("local vars ", i, j, q, w, k, blah)
 
 	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
 	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
@@ -72,15 +74,25 @@ func main() {
 	l := float64(i)
 	fmt.Println("conversão de números ", i, l)
 
-	fmt.Println("Happy", Blah, "Day")
+	fmt.Println("conts ", Blah, Bleh)
 }
+
+// go run tutorial__part1__basic.go
 
 // PACKAGES
 // - Every Go program is made up of packages.
 // - Programs start running in package main.
-// - By convention, the package name is the same as the last element of the import path. For instance,
-// the "math/rand" package comprises files that begin with the statement package rand.
+// - By convention, the package name is the same as the last element of the import
+// path. For instance, the "math/rand" package comprises files that begin with the
+// statement package rand.
 // - In Go, a name is exported if it begins with a capital letter.
+
+// ASSIGNMENT
+// - In Go, := is for declaration + assignment, whereas = is for assignment only.
+// - For example, var foo int = 10 is the same as foo := 10.
+//
+// - Outside a function, every statement begins with a keyword (var, func, and
+// so on) and so the := construct is not available.
 
 // TYPES
 // - bool
@@ -90,5 +102,3 @@ func main() {
 // - rune // alias for int32, represents a Unicode code point
 // - float32 float64
 // - complex64 complex128
-
-// Next: https://tour.golang.org/flowcontrol/1
---
+++ Golang/Tutorial/tutorial__part2__flowcontrol.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to the playground, part 2!")
+}
+
+// Next: https://tour.golang.org/flowcontrol/1
